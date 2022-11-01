9c3c8c1e5a712e4d47531e8c17c113f22e7a8cef
Snir Elgabsi <elsnir@gmail.com>
2022-11-01 12:53:50 +0200

    Add project files.

---
+++ SignalREchoClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+
+namespace SignalREchoClient
+{
+	class Program
+	{
+		private static string EchoUrlString = "sdms";
+		private const string ServiceUri = "http://localhost:12722/";
+
+		static void Main(string[] args)
+		{
+			var connection = new Connection(ServiceUri + EchoUrlString, "name=michal");
+			connection.Received += connection_Received;
+			connection.StateChanged += connection_StateChanged;
+
+			Console.WriteLine("Connecting...");
+
+			connection.Start().Wait();
+
+			string inputMsg;
+			while (!string.IsNullOrEmpty(inputMsg = Console.ReadLine()))
+			{
+				connection.Send(inputMsg).Wait();
+			}
+
+			connection.Stop();
+		}
+
+		static void connection_StateChanged(StateChange state)
+		{
+			if (state.NewState == ConnectionState.Connected)
+			{
+				Console.WriteLine("Connected.");
+			}
+		}
+
+		static void connection_Received(string data)
+		{
+			Console.WriteLine(data);
+		}
+	}
+}
---
+++ SignalREchoServer/MyConnection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalREchoServer
+{
+    public class MyConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            string msg = string.Format("A new user {0} has just joined. (ID: {1})", request.QueryString["name"], connectionId);
+            return Connection.Broadcast(msg);
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            // Broadcast data to all clients
+            string msg = string.Format("{0}: {1}", request.QueryString["name"], data);
+            return Connection.Broadcast(msg);
+        }
+    }
+}
